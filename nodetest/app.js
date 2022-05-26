2fba48ba2d50091864e15edaf4c17c6021bf5bd2
AkinoTaisei <takino0309@gmail.com>
2022-05-26 22:39:42 +0900

    再帰

--- nodetest/app.js
+++ nodetest/app.js
@@ -1,5 +1,5 @@
 'use strict'
-function fib(a){
+/*function fib(a){
     if(a<=2){
         return 1;
     }else{
@@ -9,4 +9,14 @@ function fib(a){
 
 for(var i=1; i<=process.argv[2]; i++){
     console.log(fib(i));
+}*/
+
+var a=new Array();
+for(var i=0; i<process.argv[2]; i++){
+    if(i>1){
+        a.push(a[i-1]+a[i-2]);
+    }else{
+        a.push(1);
+    }
+    console.log(a[i]);
 }
